c0ef42dc900de6d7014dfe748e864c68539d5d58
Andrew Willmott <awillmott+git@gmail.com>
2025-09-02 11:46:26 +0100

    warning fix

--- tiny_ocl.h
+++ tiny_ocl.h
@@ -418,7 +418,7 @@ void FatalError( const char* fmt, ... )
 #ifdef _WINDOWS_ // i.e., windows.h has been included.
 	MessageBox( NULL, t, "Fatal error", MB_OK );
 #else
-	fprintf( stderr, t );
+	fprintf( stderr, "%s", t );
 #endif
 	while (1) exit( 0 );
 }
